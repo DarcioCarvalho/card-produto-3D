e0ba34df4bb61bbe833bf8fde916125c806b3b62
DarcioCarvalho <darcio.nuno@gmail.com>
2023-01-18 10:50:56 -0300

    add card-produto-html

---
+++ card-produto-html/index.js
@@ -0,0 +1,16 @@
+function activeAnimation() {
+  const images = document.querySelector("#image").children;
+  const buttons = document.querySelector("#button").children;
+
+  for (const item of images) {
+    item.classList.toggle('hide-image');
+  }
+  for (const item of buttons) {
+    item.classList.toggle('hide-button');
+  }
+
+
+  console.log(images);
+}
+
+
